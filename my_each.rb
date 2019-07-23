f16a788fd36040701614a70a6fe894d164ec7755
LaNice Powell <lanicemp@aol.com>
2019-07-23 15:27:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,25 +1,12 @@
 require "pry"
 
 def my_each(greetings)
- i =0
-while i < array.length 
-yeild(array[i])
-i= i + 1 
+  i =0
+  while i < array.length 
+    yeild(array[i])
+    i= i + 1 
+  end 
+  array 
 end 
-a
 
 
-=begin
- if block_given?
- yield array[i]
- i = 0
- 
-  while i < array.length
-    i = i + 1
-  end
-  array
-else 
-  puts "Hey! No block was given!"
-  end
-end
-=stop 

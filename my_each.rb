826010eb4f368aa6bad51c64ee52af870621e5e5
LaNice Powell <lanicemp@aol.com>
2019-07-23 13:39:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,31 @@
-def my_each # put argument(s) here
-  # code here
+require "pry"
+
+def my_each (greetings)
+if block_given?
+ yield array[i]
+ i = 0
+ 
+  while i < array.length
+    i = i + 1
+  end
+  array
+else 
+  puts "Hey! No block was given!"
+  end 
+ binding.pry 
 end
+
+=begin
+ if block_given?
+ yield array[i]
+ i = 0
+ 
+  while i < array.length
+    i = i + 1
+  end
+  array
+else 
+  puts "Hey! No block was given!"
+  end
+end
+=stop 
